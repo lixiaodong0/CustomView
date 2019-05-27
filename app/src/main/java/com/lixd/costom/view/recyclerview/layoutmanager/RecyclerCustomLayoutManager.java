e95167c5910f99e84d3c3de9dab3ccd9aee8ac64
lixiaodong <lixiaodong@ggn.top>
2019-05-27 10:39:53 +0800

    自定义LayoutManager

--- app/src/main/java/com/lixd/costom/view/recyclerview/layoutmanager/RecyclerCustomLayoutManager.java
+++ app/src/main/java/com/lixd/costom/view/recyclerview/layoutmanager/RecyclerCustomLayoutManager.java
@@ -49,7 +49,7 @@ public class RecyclerCustomLayoutManager extends RecyclerView.LayoutManager {
         }
 
         //当所有item的高度没有超过 RecyclerView高度时, 取RecyclerView高度
-        totalHeight = Math.max(top, getVerticalSpace());
+        mTotalHeight = Math.max(top, getVerticalSpace());
     }
 
     /**
@@ -70,9 +70,9 @@ public class RecyclerCustomLayoutManager extends RecyclerView.LayoutManager {
     }
 
     //滑动的总高度
-    private int totalScrollHeight;
+    private int mTotalScrollHeight;
     //Item总高度
-    private int totalHeight;
+    private int mTotalHeight;
 
     /**
      * 纵向滑动的偏移量
@@ -83,12 +83,12 @@ public class RecyclerCustomLayoutManager extends RecyclerView.LayoutManager {
     public int scrollVerticallyBy(int dy, RecyclerView.Recycler recycler, RecyclerView.State state) {
         int scrollOffset = dy;
 
-        if (totalScrollHeight + scrollOffset < 0) {
+        if (mTotalScrollHeight + scrollOffset < 0) {
             //滑动到顶部 需要越界处理
-            scrollOffset = -totalScrollHeight;
-        } else if (totalScrollHeight + scrollOffset > totalHeight - getVerticalSpace()) {
+            scrollOffset = -mTotalScrollHeight;
+        } else if (mTotalScrollHeight + scrollOffset > mTotalHeight - getVerticalSpace()) {
             //滑动到底部 需要越界处理
-            scrollOffset = totalHeight - getVerticalSpace() - totalScrollHeight;
+            scrollOffset = mTotalHeight - getVerticalSpace() - mTotalScrollHeight;
         }
 
 
@@ -101,14 +101,72 @@ public class RecyclerCustomLayoutManager extends RecyclerView.LayoutManager {
                     removeAndRecycleView(child, recycler);
                     continue;
                 }
+            } else if (scrollOffset < 0) {
+                //需要回收当前屏幕，下越界的View
+                if (getDecoratedTop(child) - scrollOffset > getHeight() - getPaddingBottom()) {
+                    removeAndRecycleView(child, recycler);
+                    continue;
+                }
             }
         }
 
+        if (scrollOffset > 0) {
+            //获取屏幕可见的距离
+            Rect visibleArea = getVisibleArea(scrollOffset);
+            //获取屏幕最后一个View
+            View lastView = getChildAt(getChildCount() - 1);
+            //获取最后一个View下一个View
+            int nextPosition = getPosition(lastView) + 1;
+            //从下一个View开始遍历,如果有相交部分就布局出来
+            for (int i = nextPosition; i < getItemCount() - 1; i++) {
+                Rect rect = mItemRects.get(i);
+                if (Rect.intersects(visibleArea, rect)) {
+                    View showView = recycler.getViewForPosition(i);
+                    addView(showView);
+                    measureChildWithMargins(showView, 0, 0);
+                    layoutDecoratedWithMargins(showView, rect.left, rect.top - mTotalScrollHeight, rect.right, rect.bottom - mTotalScrollHeight);
+                } else {
+                    break;
+                }
+            }
+        } else {
+            //获取屏幕可见的距离
+            Rect visibleArea = getVisibleArea(scrollOffset);
+            //获取屏幕第一个View
+            View firstView = getChildAt(0);
+            //获取第一个View的上一个
+            int maxPosition = getPosition(firstView) - 1;
+            //从下一个View开始遍历,如果有相交部分就布局出来
+            for (int i = maxPosition; i >= 0; i--) {
+                Rect rect = mItemRects.get(i);
+                if (Rect.intersects(visibleArea, rect)) {
+                    View showView = recycler.getViewForPosition(i);
+                    addView(showView);
+                    measureChildWithMargins(showView, 0, 0);
+                    layoutDecoratedWithMargins(showView, rect.left, rect.top - mTotalScrollHeight, rect.right, rect.bottom - mTotalScrollHeight);
+                } else {
+                    break;
+                }
+            }
+        }
 
-        totalScrollHeight += scrollOffset;
+        mTotalScrollHeight += scrollOffset;
         //offsetChildrenVertical() 用于滚动所有的子View一段距离
         offsetChildrenVertical(-scrollOffset);
         return dy;
+    }
+
+    /**
+     * 获取可见的区域
+     *
+     * @param scrollOffset 当前滚动的偏移量
+     * @return
+     */
+    private Rect getVisibleArea(int scrollOffset) {
+        return new Rect(getPaddingLeft(),
+                getPaddingTop() + mTotalScrollHeight + scrollOffset,
+                getWidth() + getPaddingRight(),
+                getVerticalSpace() + mTotalScrollHeight + scrollOffset);
     }
 
     @Override
